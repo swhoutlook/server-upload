6520971a2eb87dfc81ece5c8e2c8c0c3992c79f7
axwhyzee <siah_weehung@yahoo.com>
2023-07-25 23:24:46 +0800

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,3 @@
+QUERY_API_URL="https://1zyc2hqr0b.execute-api.ap-southeast-1.amazonaws.com/testing"
+CREATE_DB_API_URL="https://6vjnghaznf.execute-api.ap-southeast-1.amazonaws.com/create_database"
+DEFAULT_TABLE_NAME="DefaultTable"
